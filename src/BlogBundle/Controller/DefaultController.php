b2e070d32594b7085d8c071b39f3056e3f97f3c8
Luciana HEmbert <contact@lucianahembert.com>
2018-02-02 16:40:04 +0100

    style and all pages

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -12,26 +12,18 @@ class DefaultController extends Controller
       //print "In BaseClass constructor\n";
     }
 
-
+ 
     /**
-     * @Route("/blog")
+     * @Route("/operateurs", name="accueil_homepage")
      */
-    public function indexAction()
-    {
-        return $this->render('BlogBundle:Default:index.html.twig');
-    }
-
-    /**
-     * @Route("/operateurs")
-     */
-    public function testOperateurs(){
+    public function indexAction(){
 
       $bar = true;
       $str = "TEST". ($bar ? 'true' : 'false') ."TEST";
       //echo $str;
       setcookie("siteLuciana","entrou",time());
 
-      return $this->render('BlogBundle/elements.html.twig',
+      return $this->render('BlogBundle/front.html.twig',
           array('str' => $str));
 
 	  /*return $this->render('default/index.html.twig', [
@@ -39,5 +31,99 @@ class DefaultController extends Controller
         ]);*/
 
     }
+
+    /**
+     * @Route("/lepiano", name="lepiano_root") 
+     */
+    public function lepianoAction(){
+
+      return $this->render('BlogBundle/generics.html.twig');
+
+	  /*return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);*/
+
+    }
+
+
+	/**
+     * @Route("/cours", name="cours_root")
+     */
+    public function coursAction(){
+
+      return $this->render('BlogBundle/elements.html.twig');
+
+	  /*return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);*/
+
+    }
+
+    /**
+     * @Route("/villalobos", name="villalobos_root")
+     */
+    public function villaAction(){
+
+      return $this->render('BlogBundle/villalobos.html.twig');
+
+	  /*return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);*/
+
+    }
+
+    /**
+     * @Route("/cadences", name="cadences_root")
+     */
+    public function cadencesAction(){
+
+      return $this->render('BlogBundle/cadences.html.twig');
+
+	  /*return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);*/
+
+    }
+
+    /**
+     * @Route("/accords", name="accords_root")
+     */
+    public function accordsAction(){
+
+      return $this->render('BlogBundle/accords.html.twig');
+
+	  /*return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);*/
+
+    }
+
+    /**
+     * @Route("/rythme", name="rythme_root")
+     */
+    public function rythmeAction(){
+
+      return $this->render('BlogBundle/rythme.html.twig');
+
+	  /*return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);*/
+
+    }
+
+    /**
+     * @Route("/terminologie", name="terminologie_root")
+     */
+    public function terminologieAction(){
+
+      return $this->render('BlogBundle/terminologie.html.twig');
+
+	  /*return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);*/
+
+    }
+
+
 
 }
